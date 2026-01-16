9b5800d60d005e25f1fbc50e68cd188fb7243eb8
sythoniq <188770367+sythoniq@users.noreply.github.com>
2026-01-16 22:06:40 +0300

    attempting to add hash function, minor errors with c referencing it

--- c/src/hash_table.c
+++ c/src/hash_table.c
@@ -1,7 +1,19 @@
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include "./hash_table.h"
+
+int ht_hash(const char* s, const int a, const int m) {
+  long hash = 0;
+  const int len_s = strlen(s);
+  for(int i = 0; i < len_s; i++) {
+    hash += (long)pow(a, len_s - (i+1)) * s[i];
+    hash = hash % m;
+  }
+
+  return (int)hash;
+}
 
 static ht_item* ht_new_item(const char* k, const char* v) {
     ht_item* i = malloc(sizeof(ht_item));
